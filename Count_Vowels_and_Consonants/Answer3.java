2f362ee498e6203950e0862c160c0cad822d088b
Aadhith Sakthivel <aadhith.ad@gmail.com>
2022-04-19 19:03:43 +0530

    Q3 Updated But some test cases failed

--- Count_Vowels_and_Consonants/Answer3.java
+++ Count_Vowels_and_Consonants/Answer3.java
@@ -1,60 +1,29 @@
 import java.util.*;
-public class MyClass {
-      public static void main(String[] args)
-      {
-    	  Scanner get = new Scanner(System.in);
-    	  int n = get.nextInt();
-    	  List<Double> lst= new ArrayList<Double>();
-    	  for(int i = 0; i<n; i++)
-    	  {
-    		  double x = get.nextDouble();
-    		 
-    		  lst.add(x);
-    	  }
-    	  
-    	  
-    	 List <Double> ans1 = findValidScore(lst);
-    	 
-    	 List <Integer> fynl = validateScore(ans1);
-    	 
-    
-    
-    	  if(fynl.isEmpty())
-    		  System.out.print("No valid Scores");
-    		  
-    	  else
-    		  System.out.print("Valid Scores = "+fynl);
-    	  
-    	  get.close();
-      }
-      
-	public static List<Double> findValidScore(List<Double> lst)
-      
-	  {
-    	  List <Double> temp =  new ArrayList<Double>();
-    	  for(int i=0; i<lst.size();i++)
-    	  {
-    		  if(lst.get(i)>0 && lst.get(i)<=100)
-    		  {
-    			  temp.add(lst.get(i));
-    		  }
-    	  }
-		return temp;
-		  
-	  }
-	private static List<Integer> validateScore(List<Double> ans1) {
-		List <Integer> temp = new ArrayList<Integer>();
-		 for(int i=0; i<ans1.size();i++)
-			 
-   	  {
-			 
-   		  if(ans1.get(i)==ans1.get(i).intValue())
-   		  {
-   			  int x = ans1.get(i).intValue();
-   			  temp.add(x);
-   		  }
-   	  }
-		return temp;
-	}
-}
+public class MyClass 
+{
+    public static void main(String[] args) 
+    {    
+	    Scanner get = new Scanner(System.in);
+            String str = get.nextLine();
 
+            int vCount = 0, cCount = 0;    
+            str = str.toLowerCase();    
+
+            for(int i = 0; i < str.length(); i++) 
+            {    
+                if(str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'i' || str.charAt(i) == 'o' || str.charAt(i) == 'u') 
+                {    
+                    vCount++;    
+                }    
+
+                else if(str.charAt(i) > 'a' && str.charAt(i)<='z') 
+                {      
+
+                    cCount++;    
+                }    
+            }    
+            System.out.println("Number of Vowels:" + vCount);    
+            System.out.println("Number of Consonants:" + cCount);  
+       get.close();
+    }       
+}
